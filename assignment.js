b3d56ecbdb0b5a31cddffb50c0510f24d5e3332f
nasir uddin <nasirbdctg@yahoo.com>
2021-01-22 12:58:16 +0000

    two commit

--- assignment.js
+++ assignment.js
@@ -6,3 +6,27 @@ function kilometerToMeter(kilometer){
 }
 var result = kilometerToMeter(213);
 console.log(result);
+
+//project three
+function hotelCost(day) {
+    var cost = 0;
+    if (day <= 10) {
+        cost = day * 100;
+    }
+    else if (day <= 20) {
+        var firstPart = 10 * 100;
+        var remaining = day - 10;
+        var secondPart = remaining * 80;
+        cost = firstPart + secondPart;
+    }
+    else {
+        var firstPart = 10 * 100;
+        var secondPart = 10 * 80;
+        var remaining = day - 20;
+        var thirdPart = remaining * 50;
+        cost = firstPart + secondPart + thirdPart;
+    }
+    return cost;
+}
+var count = hotelCost(32);
+console.log(count);
